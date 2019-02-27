b428df5f454fc9270b4aa37bd1a3802b1ca5d8ed
ekafaradewi <44692821+ekafaradewi@users.noreply.github.com>
2019-02-27 20:07:56 +0700

    'RecyclerViewKetiga'

--- app/src/main/java/com/example/ekarecyclerview/models/Resep.java
+++ app/src/main/java/com/example/ekarecyclerview/models/Resep.java
@@ -1,4 +1,66 @@
 package com.example.ekarecyclerview.models;
 
 public class Resep {
+
+    private String id;
+    private String title;
+    private String desc;
+    private String img;
+    private int img_local;
+
+    public Resep(String id, String title, String desc, String img) {
+        this.id = id;
+        this.title = title;
+        this.desc = desc;
+        this.img = img;
+    }
+
+    public Resep(String id, String title, String desc, int img_local) {
+        this.id = id;
+        this.title = title;
+        this.desc = desc;
+        this.img_local = img_local;
+    }
+
+    public String getId() {
+
+        return id;
+    }
+
+    public void setId(String id) {
+
+        this.id = id;
+    }
+
+    public String getTitle() {
+
+        return title;
+    }
+
+    public void setTitle(String title) {
+
+        this.title = title;
+    }
+
+    public String getImg() {
+
+        return img;
+    }
+
+    public void setImg(String img) {
+
+        this.img = img;
+    }
+
+    public int getImg_local() {
+
+        return img_local;
+    }
+
+    public void setImg_local(int img_local) {
+
+        this.img_local = img_local;
+    }
+
+
 }
